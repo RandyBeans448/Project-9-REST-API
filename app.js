afe196015e3a25942e8c615d798c4b143d1e59f1
RandyBeans448 <65494300+RandyBeans448@users.noreply.github.com>
2020-07-28 21:54:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const { Sequelize } = require('sequelize');
-const bcrypt = require('bcryptjs');
+const bodyParser = require('body-parser');
 
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
@@ -38,7 +38,10 @@ async function authenticate () {
 
 // TODO setup your api routes here
 
+app.use(bodyParser.json());
+
 app.use('/api', courseRoutes, userRoutes);
+
 
 
 // setup a friendly greeting for the root route
@@ -74,4 +77,6 @@ app.set('port', process.env.PORT || 5000);
 const server = app.listen(app.get('port'), () => {
   console.log(`Express server is listening on port ${server.address().port}`);
 });
+
+
 
