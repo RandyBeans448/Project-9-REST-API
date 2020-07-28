05a96554fc01132ff1cf5a0d21d6d0a34db25996
RandyBeans448 <65494300+RandyBeans448@users.noreply.github.com>
2020-07-28 21:50:51 +0100

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,36 +4,65 @@ const router = express.Router();
 const Sequelize = require('sequelize');
 const User = require('../models').User;
 const bcrypt = require('bcryptjs');
+const auth = require('basic-auth');
+
+
+
 
 function asyncHandler(callback){
     return async(req, res, next) => {
       try {
         await callback(req, res, next)
       } catch(error){
-        res.status(500).render('error');
+        res.status(500);
       }
     }
   }
 
-  bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash("B4c0/\/", salt, function(err, hash) {
-        // Store hash in your password DB.
-    });
-  });
+  const authenticateUser = (req, res, next) => {
+    let message = null;
+    const credentials = auth(req);
+        if (credentials) {
+          const user = User.findOne({ where: { email: req.body.email } })
+            .compareSync(credentials.password, user.password);
+              if (user) {
+                console.log(`Authentication successful for ${user.firstName}`);
+                req.currentUser = user;
+              } else {
+                message = `Authentication failure for ${user.firstName}`;
+              }
+          } 
+            if (message) {
+              console.warn(message);
+              res.status(401).json({ message: 'Access Denied' });
+            } else {
+              next();
+            }
+      }; 
 
-router.get('/api/users', asyncHandler(async (req, res) => {
+
+
+
+router.get('/users', asyncHandler, authenticateUser(async (req, res) => {
     const user = await User.findByPk(req.params.id);
+    bcrypt.compare(req.body.password, hash, function(err, result) {
+  });
     res.sendStatus(200);
   }));
   
-  router.post('/api/users', asyncHandler(async (req, res, next) => {
+  router.post('/users', asyncHandler(async (req, res, next) => {
     let user;
     try {
       user = await User.create({
         firstName: req.body.title,
         lastName: req.body.lastName,
         email: req.body.email,
-        password: req.body.password })
+        password: bcrypt.genSalt(10, function(err, salt) {
+          bcrypt.hash(req.body.password, salt, function(err, hash) {
+          });
+        })
+       })
+        
         res.sendStatus(201).setHeader("Location", '/');
     } catch(error) {
       if(error.name === 'SequelizeValidationError') {
