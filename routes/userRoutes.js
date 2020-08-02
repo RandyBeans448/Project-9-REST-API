fdbcd7654a10b71fb6528287ce1e5f374bcc5ff6
RandyBeans448 <65494300+RandyBeans448@users.noreply.github.com>
2020-08-02 17:33:19 +0100

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,9 +6,6 @@ const User = require('../models').User;
 const bcrypt = require('bcryptjs');
 const auth = require('basic-auth');
 
-
-
-
 function asyncHandler(callback){
     return async(req, res, next) => {
       try {
@@ -20,57 +17,61 @@ function asyncHandler(callback){
   }
 
   const authenticateUser = (req, res, next) => {
-    let message = null;
+    //Parsing Authorization Header from the request
     const credentials = auth(req);
-        if (credentials) {
-          const user = User.findOne({ where: { email: req.body.email } })
-            .compareSync(credentials.password, user.password);
-              if (user) {
-                console.log(`Authentication successful for ${user.firstName}`);
+    //If the credentials are exsist then they are compare to an email matching the first name.  
+      if (credentials) {
+        const user = User.findOne({ where: { email: credentials.firstName } })
+        //When true the user has its password comfirmed  
+          if (user) {
+            const authenticated = bcryptjs
+              .compareSync(credentials.pass, user.password);
+              //Then is the user is set the current User in the request object
+              if (authenticated) {
                 req.currentUser = user;
               } else {
-                message = `Authentication failure for ${user.firstName}`;
+                console.log(`Authentication failed for username: ${user.firstName}`);
               }
-          } 
-            if (message) {
-              console.warn(message);
-              res.status(401).json({ message: 'Access Denied' });
-            } else {
-              next();
-            }
-      }; 
+          } else {
+            console.log(`User not found with the name of ${credentials.firstName}`)
+          }
+      } else {
+        console.log("Autho not found");
+      }
+    next();
+  };
 
 
 
 
-router.get('/users', authenticateUser, asyncHandler(async (req, res) => {
+router.get('/users', asyncHandler(async (req, res) => {
     const user = await User.findByPk(req.params.id);
-    bcrypt.compare(req.body.password, hash, function(err, result) {
-  });
-    res.sendStatus(200);
+    // bcrypt.compare(req.body.password, hash, function(err, result) {});
+    console.log(user);
+    res.json({ user }).sendStatus(200);
   }));
   
-  router.post('/users', asyncHandler(async (req, res, next) => {
-    let user;
-    try {
-      user = await User.create({
-        firstName: req.body.title,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: bcrypt.genSalt(10, function(err, salt) {
-          bcrypt.hash(req.body.password, salt, function(err, hash) {
-          });
-        })
-       })
-        
-        res.sendStatus(201).setHeader("Location", '/');
-    } catch(error) {
-      if(error.name === 'SequelizeValidationError') {
-        user = await User.build(req.build);
-      } else {
-        throw error;
-      }
-    }
- }));
+//   router.post('/users', asyncHandler(async (req, res, next) => {
+//     let user;
+//     try {
+//       user = await User.create({
+//         firstName: req.body.title,
+//         lastName: req.body.lastName,
+//         email: req.body.email,
+//         password: bcrypt.genSalt(10, function(err, salt) {
+//           bcrypt.hash(req.body.password, salt, function(err, hash) {
+//           });
+//         })        
+//        })
+//        users.push(user);
+//         res.sendStatus(201).setHeader("Location", '/');
+//     } catch(error) {
+//       if(error.name === 'SequelizeValidationError') {
+//         user = await User.build(req.build);
+//       } else {
+//         throw error;
+//       }
+//     }
+//  }));
 
   module.exports = router;
