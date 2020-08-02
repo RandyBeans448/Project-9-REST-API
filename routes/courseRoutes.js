11a84150d9deb16afd879763beeafbc63b08bf4a
RandyBeans448 <65494300+RandyBeans448@users.noreply.github.com>
2020-08-02 17:33:52 +0100

    Update courseRoutes.js

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const Sequelize = require('sequelize');
 const User = require('../models/User');
 const Course = require('../models').Course;
-
+const auth = require('basic-auth');
+const bcrypt = require('bcryptjs');
 
 
 function asyncHandler(callback){
@@ -17,61 +18,76 @@ function asyncHandler(callback){
     }
   }
 
-router.get('/courses', asyncHandler(async (req, res, next) => {
-    const course = await Course.findAll({ 
-      order: [['createdAt', 'DESC']],
-      include: { model: User,
-        through: {
-          attributes: ['id', 'firstfirstName', 'lastName', 'email']
-         }
-      }});
-    res.sendStatus(200).end();
-  }));
-
-  router.get('/courses/:id', asyncHandler(async (req, res) => {
-    const course = await Course.findByPk(req.params.id,
-       { include: {
-          model: User,
-           through: {
-            attributes: ['id', 'firstfirstName', 'lastName', 'email']
-           }       
-      }});
-    res.sendStatus(200);
-  }));
-  
-router.post('/courses/:id', asyncHandler(async (req, res, next) => {
-     let course;
-     try {
-      course = await Course.create({
-        title: req.body.title,
-        description: req.body.description,
-        estimatedTime: req.body.estimatedTime,
-        materialsNeeded: req.body.materialsNeeded })
-        res.sendStatus(201).setHeader("Location", '/');
-     } catch(error){
-      if(error.name === 'SequelizeValidationError') {
-        course = await Course.build(req.build);
+  const authenticateUser = (req, res, next) => {
+    //Parsing Authorization Header from the request
+    const credentials = auth(req);
+    //If the credentials are exsist then they are compare to an email matching the first name.  
+      if (credentials) {
+        const user = User.findOne({ where: { email: credentials.name } })
+        //When true the user has its password comfirmed  
+          if (user) {
+            const authenticated = bcryptjs
+              .compareSync(credentials.pass, user.password);
+              //Then is the user is set the current User in the request object
+              if (authenticated) {
+                req.currentUser = user;
+              } else {
+                console.log(`Authentication failed for username: ${user.firstName}`);
+              }
+          } else {
+            console.log(`User not found with the name of ${credentials.firstName}`)
+          }
       } else {
-        throw error;
+        console.log("Autho not found");
       }
-     }
-  }));
+    next();
+  };  
 
-router.put('/courses/:id', asyncHandler(async (req, res, next) => {
-    let course = await Course.findByPk(req.params.id);
-      if(course) {
-          await course.update(req.body);
-          res.sendStatus(204).redirect('/'); 
-        } else {
-          res.sendStatus(404);
-        }
+router.get('/courses', asyncHandler(async (req, res, next) => {
+    const courses = await Course.findAll();
+      console.log(courses);
+      res.json({ courses }).sendStatus(200);
 }));
 
-router.delete('/courses/:id', asyncHandler(async (req ,res) => {
-    let course = await Course.findByPk(req.params.id)
-      await course.destroy();
-        res.sendStatus(204);
-  }));
+router.get('/courses/:id', authenticateUser, asyncHandler(async (req, res) => {
+    const course = await Course.findByPk(req.params.id);
+      console.log(course);
+      res.json({ course }).sendStatus(200);
+}));
+  
+// router.post('/courses/:id', asyncHandler(async (req, res, next) => {
+//      let course;
+//      try {
+//       course = await Course.create({
+//         title: req.body.title,
+//         description: req.body.description,
+//         estimatedTime: req.body.estimatedTime,
+//         materialsNeeded: req.body.materialsNeeded })
+//         res.sendStatus(201).setHeader("Location", '/');
+//      } catch(error){
+//       if(error.name === 'SequelizeValidationError') {
+//         course = await Course.build(req.build);
+//       } else {
+//         throw error;
+//       }
+//      }
+//   }));
+
+// router.put('/courses/:id', asyncHandler(async (req, res, next) => {
+//     let course = await Course.findByPk(req.params.id);
+//       if(course) {
+//           await course.update(req.body);
+//           res.sendStatus(204).redirect('/'); 
+//         } else {
+//           res.sendStatus(404);
+//         }
+// }));
+
+// router.delete('/courses/:id', asyncHandler(async (req ,res) => {
+//     let course = await Course.findByPk(req.params.id)
+//       await course.destroy();
+//         res.sendStatus(204);
+//   }));
 
  
 
